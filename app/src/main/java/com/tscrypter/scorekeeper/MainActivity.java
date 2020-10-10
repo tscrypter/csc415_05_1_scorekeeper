8256aca1d2d1fe75902a7b0b6edd712aea46776d
Tom Delaney <delaneyt1@gmail.com>
2020-10-10 12:19:04 -0400

    Add increment/decrement functionality

--- app/src/main/java/com/tscrypter/scorekeeper/MainActivity.java
+++ app/src/main/java/com/tscrypter/scorekeeper/MainActivity.java
@@ -1,13 +1,62 @@
 package com.tscrypter.scorekeeper;
 
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Member variables for the score
+    private int mScore1;
+    private int mScore2;
+
+    // Member variables for the score TextViews
+    private TextView mScoreText1;
+    private TextView mScoreText2;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find TextViews by ID
+        mScoreText1 = findViewById(R.id.score_1);
+        mScoreText2 = findViewById(R.id.score_2);
+
+    }
+
+    /**
+     * Method that handles the onClick of both the decrement buttons
+     * @param view The button view that was clicked
+     */
+    public void decreaseScore(View view) {
+        int viewId = view.getId();
+        switch (viewId){
+            case R.id.decreaseTeam1:
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.decreaseTeam2:
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    /**
+     * Method that handles the onClick of both the increment buttons
+     * @param view The button view that was clicked
+     */
+    public void increaseScore(View view) {
+        int viewId = view.getId();
+        switch (viewId){
+            case R.id.increaseTeam1:
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.increaseTeam2:
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
     }
 }
